57be46740a2789acfc1e75bd012c26e08d1c217d
D <drewpcchannel@gmail.com>
2017-11-16 13:08:58 -0700

    dropping turrets by rarity

--- data/scripts/player/cmd/summon.lua
+++ data/scripts/player/cmd/summon.lua
@@ -1,3 +1,9 @@
+-- require empty space?
+-- add upgrades
+-- bounty?
+-- gen and balance better end game turrets?  maybe guardian loot somewhat
+-- atleast an example for dropping good like turret comps
+
 package.path = package.path .. ";data/scripts/lib/?.lua"
 package.path = package.path .. ";data/scripts/?.lua"
 
@@ -7,11 +13,13 @@ local PlanGenerator = require ("plangenerator")
 local ShipUtility = require ("shiputility")
 local TurretGenerator = require ("turretgenerator")
 
+
 if onServer() then
 
 function initialize()
 	player = Player()
 	local str = "Testing ship summon"
+	local lastSector = {}
 	player:sendChatMessage("Debug", 0, str)
 
 	--local faction = Galaxy():createRandomFaction(Sector():getCoordinates())
@@ -36,13 +44,45 @@ function initialize()
 	ship.name = "something2"
 	ship.crew = ship.minCrew
 	ship:addScript("ai/patrol.lua")
+	ship:registerCallback("onEntityDestroyed", "onShipDestroyed")
 
 	TurretGenerator.initialize(random():createSeed())
 	local turret = TurretGenerator.generateArmed(x, y)
-	local numTurrets = math.max(2, Balancing_GetEnemySectorTurrets(x, y) * 0.75)
+	-- Number of turets gets reduced and replaced with a damage multiplier
+	local numTurrets = 3000
 
 	ShipUtility.addTurretsToCraft(ship, turret, numTurrets)
 
+	--this is validation found in the4 script that i didn't want to mess with 
+	-- make sure this is all happening in the same sector
+    local x, y = Sector():getCoordinates()
+    if lastSector.x ~= x or lastSector.y ~= y then
+        -- this must be set in order to drop the loot
+        -- if the sector changed, simply unset it
+        lastPosition = nil
+    end
+    lastSector.x = x
+    lastSector.y = y
+
+    Loot(ship.index):insert(InventoryTurret(TurretGenerator.generate(x, y, 0, Rarity(RarityType.Exotic), WeaponType.RepairBeam)))
+
 	terminate()
 end
+
+function onShipDestroyed(shipIndex)
+	player:sendChatMessage("Debug", 0, "ondestroyed hits")
+
+    ships[shipIndex.string] = nil
+
+    local ship = Entity(shipIndex) 
+    local damagers = {ship:getDamageContributorPlayers()}
+    for i, v in pairs(damagers) do
+        participants[v] = v
+        player:sendChatMessage("Human Alliance", 0, "Thank you for destoying this beast.")
+    	player:receive(3000000)
+    end
+
+    -- if they're all destroyed, the event ends
+end
+
 end

5f9d78601304fb0b66a040b1701b8715a2d24abc
D <drewpcchannel@gmail.com>
2017-11-17 15:07:21 -0700

    added in config

--- data/scripts/player/cmd/summon.lua
+++ data/scripts/player/cmd/summon.lua
@@ -7,8 +7,10 @@ package.path = package.path .. ";data/scripts/lib/?.lua"
 package.path = package.path .. ";data/scripts/?.lua"
 
 function initialize()
+	local players = {Sector():getPlayers()}
+	for _, player in pairs(players) do
+		player:sendChatMessage("Server", 0, "This sector will now spawn a boss upon entry")
+	end
     Sector():addScript("player/cmd/bossSector.lua")
 	terminate()
 end
-
-

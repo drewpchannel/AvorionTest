d01604b55c50de60f3e6f5dcddbd19e4a50e87b5
D <drewpcchannel@gmail.com>
2017-11-17 14:07:18 -0700

    code cleanup

--- data/scripts/player/cmd/summon.lua
+++ data/scripts/player/cmd/summon.lua
@@ -7,7 +7,6 @@ package.path = package.path .. ";data/scripts/lib/?.lua"
 package.path = package.path .. ";data/scripts/?.lua"
 
 function initialize()
-	print("DB 5 trying to set sector with command")
     Sector():addScript("player/cmd/bossSector.lua")
 	terminate()
 end

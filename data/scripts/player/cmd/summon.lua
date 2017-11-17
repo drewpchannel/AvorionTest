ed821010f332971850eb4b31a52d5490e984a95a
D <drewpcchannel@gmail.com>
2017-11-17 14:06:16 -0700

    Sectors can be set and boss spawns in

--- data/scripts/player/cmd/summon.lua
+++ data/scripts/player/cmd/summon.lua
@@ -6,43 +6,9 @@
 package.path = package.path .. ";data/scripts/lib/?.lua"
 package.path = package.path .. ";data/scripts/?.lua"
 
-require ("galaxy")
-require ("stringutility")
-local PlanGenerator = require ("plangenerator")
-local ShipUtility = require ("shiputility")
-local TurretGenerator = require ("turretgenerator")
-
 function initialize()
 	print("DB 5 trying to set sector with command")
-	player = Player()
-	local factionName = "Astrayanians 44"
-	local faction = Galaxy():findFaction(factionName)
-	if Galaxy():findFaction(factionName) == nil then
-	    faction = Galaxy():createFaction(factionName, 310, 0)
-	    faction.initialRelations = 0
-	    faction.initialRelationsToPlayer = -100000
-	    faction.staticRelationsToPlayers = true
-	end
-
-	local plan = LoadPlanFromFile("data/plans/Idk3.xml")
-	local pos = random():getVector(-1000, 1000)
-    pos = MatrixLookUpPosition(-pos, vec3(0, 1, 0), pos)
-
-	local ship = Sector():createShip(faction, "Astrayas Class", plan, pos) 
-	ship.title = "something"
-	ship.name = "something2"
-	ship.crew = ship.minCrew
-	ship:addScript("ai/patrol.lua")
-	ship:addScript("player/cmd/bossDeath.lua")
-	Sector():addScript("player/cmd/bossSector.lua")
-
-	TurretGenerator.initialize(random():createSeed())
-	local turret = TurretGenerator.generateArmed(x, y)
-	-- Number of turets gets reduced and replaced with a damage multiplier
-	local numTurrets = 3000
-
-	ShipUtility.addTurretsToCraft(ship, turret, numTurrets)
-    Loot(ship.index):insert(InventoryTurret(TurretGenerator.generate(x, y, 0, Rarity(RarityType.Exotic), WeaponType.RepairBeam)))
+    Sector():addScript("player/cmd/bossSector.lua")
 	terminate()
 end
 
